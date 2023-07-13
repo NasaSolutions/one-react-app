3c543c7aae87f7f3fdd2d92293f827494df0ae56
mmemshima <132572740+Mmemshima@users.noreply.github.com>
2023-07-13 13:53:56 +0100

    Created React Project, linked it to GIthub

--- src/App.js
+++ src/App.js
@@ -2,5 +2,15 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by Chinasa Iweajunwa and is{" "}
+        <a href="https://github.com/NasaSolutions/one-react-app" target="blank">
+          Open sourced on Github
+        </a>
+      </footer>
+    </div>
+  );
 }
